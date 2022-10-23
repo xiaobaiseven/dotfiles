f59b8cc1382e3d003dd3e629bdb0649a3df5320f
xiaobaiseven <1146112953@qq.com>
2022-10-23 17:43:16 +0800

    dc-slstatus:added music play info

--- scripts/get_music_info.sh
+++ scripts/get_music_info.sh
@@ -4,17 +4,26 @@ dwm_get_music_info(){
         TITLE=$(playerctl metadata title)
 				STATUS=$(playerctl status)
 				SEP=""
+
 				if [ "$STATUS" = "Playing" ]; then
-                STATUS="▶"
-								SEP="-"
-								echo "$STATUS" "$ARTIST" "$SEP" "$TITLE "
+							STATUS="▶ 正在播放"
+							SEP="-"
+							if [ "$ARTIST" = "" ];then
+								echo "$STATUS"视频:"$TITLE "
+							else
+								echo "$STATUS"音乐:"$ARTIST" "$SEP" "$TITLE "
+							fi
 						elif [ "$STATUS" = "Paused" ]; then	
-								STATUS="⏸"
-								SEP="-"
-								echo "$STATUS" "$ARTIST" "$SEP" "$TITLE "
-            else
-								STATUS=""
-								echo ""
-            fi
+							STATUS="⏸ 暂停播放"
+							SEP="-"
+							if [ "$ARTIST" = "" ];then
+								echo "$STATUS"视频:"$TITLE "
+							else
+								echo "$STATUS"音乐:"$ARTIST" "$SEP" "$TITLE "
+							fi
+						else
+							STATUS=""
+						echo ""
+        fi
 }
 dwm_get_music_info
